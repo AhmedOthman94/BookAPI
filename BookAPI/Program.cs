f7037a5e9da770c6ee8769035d3dcaeaa7933a3e
AhmedOthman94 <othmanahmed7367@gmail.com>
2026-09-01 11:26:40 +0300

    AddedScalar

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,10 +1,15 @@
+using System.Text;
 using BookAPI.Data;
 using BookAPI.Middleware;
 using BookAPI.Profiles;
 using BookAPI.Services;
 using BookAPI.Services.IServices;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +23,43 @@ builder.Services.AddProblemDetails();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v1", options =>
+{
+	options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
+	options.AddDocumentTransformer((document, context, cancellationToken) =>
+	{
+		document.Info = new()
+		{
+			Title = "Book Management System API",
+			Version = context.DocumentName,
+			Description = "A robust and scalable RESTful API built with .NET 10 to manage Books and Authors using Clean Architecture principles."
+		};
+
+		document.Components ??= new OpenApiComponents();
+		document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+
+		document.Components.SecuritySchemes.Add("Bearer", new OpenApiSecurityScheme
+		{
+			Type = SecuritySchemeType.Http,
+			Scheme = "bearer",
+			BearerFormat = "JWT",
+			Description = "Input your JWT bearer token to access secured endpoints."
+		});
+
+		document.Security =
+		[
+			new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecuritySchemeReference("Bearer"),
+						[]
+					}
+				}
+		];
+
+		return Task.CompletedTask;
+	});
+});
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -29,6 +70,27 @@ builder.Services.AddAutoMapper(cfg =>
 	cfg.AddProfile<AuthorProfile>();
 });
 
+
+builder.Services.AddAuthentication(opts => 
+{
+	opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new()
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(
+							Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+		)
+	};
+});
 
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
@@ -57,6 +119,14 @@ app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+
+	app.MapScalarApiReference(options =>
+	{
+		options
+			.WithTitle("Book Management System API")
+			.WithTheme(ScalarTheme.Purple)
+			.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+	});
 }
 
 app.UseHttpsRedirection();
